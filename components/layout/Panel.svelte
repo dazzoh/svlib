a9981e7953be901cd6e1cae17d713c1060a2c55b
darren <darren@innovez.com>
2021-07-29 16:28:47 +0100

    some svg icons added

--- components/layout/Panel.svelte
+++ components/layout/Panel.svelte
@@ -1,6 +1,37 @@
-<div class="Panel">
-    <h2 class="Panel-header" >
-        <slot name="header" />
+<script>
+    import Icon from "../icon/Icon.svelte";
+    import IconEnlarge from "../icon/IconEnlarge.svelte";
+</script>
+
+<div class="panel">
+    <h2 class="header px-2 bg-gradient-to-r from-dark-400 to-dark-500  ">
+        <span class="flex-1">
+            <slot name="header" />
+        </span>
+        <!-- <Icon title="Fullscreen">
+            <IconEnlarge />
+        </Icon> -->
     </h2>
-    <slot />
+    <div class="body">
+        <slot />
+    </div>
 </div>
+
+<style lang="postcss">
+    .panel {
+        @apply bg-dark-300 rounded flex flex-col  min-h-0;
+    }
+
+    .header {
+        @apply text-light-200 flex items-center;
+        border-top-left-radius: inherit;
+        border-top-right-radius: inherit;
+        box-shadow: 0px 2px 2px #00000073;
+        
+    }
+
+    .body {
+        @apply flex-1 mt-1 flex;
+        overflow: auto;
+    }
+</style>
